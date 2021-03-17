635fcc60f4fd3594b1c978f332be991409cc5e15
Yusuf Savaş <yusufsss4@gmail.com>
2021-03-17 22:07:41 +0300

    Complex data are added

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
@@ -9,10 +7,19 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
+        [Range(0, 5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -14,6 +12,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; } 
         //Question mark means that Grade is nullable.
 
