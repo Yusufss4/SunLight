8455e16bba40daeb6cc92e0bb6213667585a677e
Yusuf Savaş <yusufsss4@gmail.com>
2021-04-28 11:51:15 +0300

    Table is now more efficient. Code clean uptade.

--- ContosoUniversity/wwwroot/js/Chart.js
+++ ContosoUniversity/wwwroot/js/Chart.js
@@ -1,46 +1,46 @@
-﻿window.onload = function () {
-	var dataPoints = [];
-	var chart;
-	$.getJSON("https://localhost:44335/api/students", function (data) {
-		$.each(data, function (key, value) {
-			dataPoints.push({ x: value.id, y: value.ldR1 });
-			console.log("DataPoints in first section->")
-			console.log(dataPoints);
-		});
-		chart = new CanvasJS.Chart("chartContainer", {
-			title: {
-				text: "LDR1 Table"
-			},
-			data: [{
-				type: "line",
-				dataPoints: dataPoints,
-			}]
-		});
-		chart.render();
-		updateChart();
-	});
-	function updateChart() {
-		console.log("dataPoints");
-		console.log(dataPoints.length);
-		$.getJSON("https://localhost:44335/api/students" + "/" + (dataPoints.length), function (value) {
+﻿function mainChart() {
+    var dataPoints = [];
+    var chart;
+    $.getJSON("https://localhost:44335/api/students", function (data) {
+        $.each(data, function (key, value) {
+            dataPoints.push({ x: value.id, y: value.ldR1 });
+            console.log("DataPoints in first section->")
+            console.log(dataPoints);
+        });
+        chart = new CanvasJS.Chart("chartContainer", {
+            title: {
+                text: "LDR1 Table"
+            },
+            data: [{
+                type: "line",
+                dataPoints: dataPoints,
+            }]
+        });
+        chart.render();
+        updateChart();
+    });
+    function updateChart() {
+        console.log("dataPoints");
+        console.log(dataPoints.length);
+        $.getJSON("https://localhost:44335/api/students" + "/" + (dataPoints.length), function (value) {
 
-			console.log("DataPoints in updateChart->")
-			console.log(dataPoints);
-			
-			
-				
-					console.log(value.id);
-					console.log(value.ldR1);
-					dataPoints.push({
-						x: value.id, y: value.ldR1
-					});
-				
-			
-			chart.render();
-			//setTimeout(function () { updateChart() }, 1000);
-		});
-	}
-	setInterval(function () {
-		updateChart();
-	}, 1000);
+            console.log("DataPoints in updateChart->")
+            console.log(dataPoints);
+
+
+
+            console.log(value.id);
+            console.log(value.ldR1);
+            dataPoints.push({
+                x: value.id, y: value.ldR1
+            });
+
+
+            chart.render();
+            //setTimeout(function () { updateChart() }, 1000);
+        });
+    }
+    setInterval(function () {
+        updateChart();
+    }, 1000);
 }
---
+++ ContosoUniversity/wwwroot/js/Table.js
@@ -0,0 +1,39 @@
+﻿console.log("TableJavaScript");
+function mainTable() {
+    console.log("In getAllMessages();");
+    var tbl = $('#messagesTable');
+    var i = 1;
+    console.log("In windowonload");
+
+    $.ajax({
+        url: '/api/students',
+        contentType: 'application/html ; charset:utf-8',
+        type: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            tbl.empty();
+            $.each(result, function (key, value) {
+                tbl.append('<tr>' + '<td>' + i + '</td>' + '<td>' + value.id + '</td>' + '<td>' + value.firstMidName + '</td>' + '<td>' + value.createDate + '</td>' + '<td>' + value.fullName + '</td>' + '</tr>');
+                i = i + 1;
+            }); updateTable();
+        }
+    });
+
+    function updateTable() {
+        console.log("uptadeTable");
+        $.getJSON("https://localhost:44335/api/students" + "/" + (i))
+            .done(function (value) {
+                console.log("JSON Data: " + value);
+                tbl.append('<tr>' + '<td>' + i + '</td>' + '<td>' + value.id + '</td>' + '<td>' + value.firstMidName + '</td>' + '<td>' + value.createDate + '</td>' + '<td>' + value.fullName + '</td>' + '</tr>');
+                i = i + 1;
+            })
+            .fail(function (jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                console.log("Request Failed: " + err);
+            });
+    }
+    setInterval(function () {
+        updateTable();
+    }, 1000);
+}
